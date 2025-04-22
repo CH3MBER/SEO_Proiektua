10b5de401900341e22e4a2b74c16fb503bf30892
ComradeTxuria <164156208+ComradeTxuria@users.noreply.github.com>
2025-04-22 13:36:54 +0200

    de la 5 a la 7

--- menu_surya.sh
+++ menu_surya.sh
@@ -6,20 +6,20 @@ function proiektufitxategiakPaketatuetaKonprimitu()
 }
 function mysqlKendu()
 {
-#Zerbitzua gelditu
-sudo systemctl stop mysql.service
-#Ezabatu paketeak +konfigurazioak +datuak
-sudo apt purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*
-#ezabatu beharrezkoak ez diren paketeak
-sudo apt autoremove
-#Cache-a garbitu
-sudo apt autoclean
-#Datuak ezabatzen
-sudo rm -rf /var/lib/mysql
-#Konfigurazioa ezabatzen
-sudo rm -rf /etc/mysql/
-#bitakora ezabatzen
-sudo rm -rf /var/log/mysql
+	#Zerbitzua gelditu
+	sudo systemctl stop mysql.service
+	#Ezabatu paketeak +konfigurazioak +datuak	
+	sudo apt purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*
+	#ezabatu beharrezkoak ez diren paketeak
+	sudo apt autoremove
+	#Cache-a garbitu
+	sudo apt autoclean	
+	#Datuak ezabatzen
+	sudo rm -rf /var/lib/mysql
+	#Konfigurazioa ezabatzen
+	sudo rm -rf /etc/mysql/
+	#bitakora ezabatzen
+	sudo rm -rf /var/log/mysql
 }
 
 function kokapenberriaSortu()
@@ -67,6 +67,38 @@ function mysqlInstalatu()
 	echo "Abiarazita dago \n"
 }
 
+function datubasekoerabiltzaileaSortu()
+{
+	if [ ! -f "datubasekoerabiltzaileasortu.sql" ]
+	then 
+		echo "CREATE USER 'lsi'@'localhost' IDENTIFIED BY 'lsi';" > "./datubasekoerabiltzaileasortu.sql"
+		echo "GRANT CREATE, ALTER, DROP, INSERT, UPDATE, INDEX, DELETE, SELECT, REFERENCES, RELOAD ON *.* TO 'lsi'@'localhost' WITH GRANT OPTION;" >> "./datubasekoerabiltzaileasortu.sql"
+		echo "FLUSH PRIVILEGES;" >> "./datubasekoerabiltzaileasortu.sql"
+		echo "Erabiltzailea sortuta \n"
+		sudo mysql < "datubasekoerabiltzaileasortu.sql"
+	else
+		echo "Erabiltzailea sortuta dago dagoeneko \n"
+	fi
+		
+}
+
+function datubaseaSortu()
+{
+	sudo mysql -u lsi -p < "/var/www/formulariocitas/script.sql"
+	echo "Sortu da taula \n"
+}
+
+function ingurunebirtualaSortu() 
+{
+	sudo apt-get update
+	sudo apt install python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+	sudo apt install python3-venv
+	sudo apt install -y python3-pip
+	python3 -m venv /var/www/formulariocitas/venv
+	source /var/www/formulariocitas/vvenv/bin/activate
+	echo "Instalatu egin da guztia \n"
+}
+
 function menutikIrten()
 {
 echo "Instalatzailearen bukaera"
@@ -76,11 +108,14 @@ opcionmenuppal=0
 while test $opcionmenuppal -ne 26
 do
     #Menua erakutsi
-    echo -e "0 Proiektu-fitxategiak paketatu eta konprimatu"
+    echo -e "0 Proiektu-fitxategiak paketatu eta konprimatu \n"
     echo -e "1 Mysql kendu \n"
     echo -e "2 Kokapen berria sortu \n" 
     echo -e "3 Proiektu fitxategiak kokapen berrian Kopiatu \n"
     echo -e "4 mysql instalatu\n"
+    echo -e "5 Datubaseko erabiltzailea sortu \n" 
+    echo -e "6 Datu basea sortu \n"
+    echo -e "7 Ingurune birtuala sortu \n"
     echo -e "26 Menutik irten \n"
     	read -p "Ze aukera egin nahi duzu?" opcionmenuppal
     case $opcionmenuppal in
@@ -89,6 +124,9 @@ do
    	 2) kokapenberriaSortu;;
    	 3) prioektufitxategiakkokapenberrianKopiatu;;
    	 4) mysqlInstalatu;;
+   	 5) datubasekoerabiltzaileaSortu;;
+   	 6) datubaseaSortu;;
+   	 7) ingurunebirtualaSortu;;
    	 26) menutikIrten;;
    	 *) ;;
     esac
